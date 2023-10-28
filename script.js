4897f4303ad7ccf9dfbbc7c908a8e49e4782e9b7
Ahmed H <hassenahmed2378@gmail.com>
2023-10-28 17:09:50 -0400

    js added

---
+++ script.js
@@ -0,0 +1,4 @@
+const addPetIn = document.getElementById("addPetB");
+const removePetIn = document.getElementById("removePetB");
+const pharmacyB = document.getElementById("pharmacyB");
+const homeB = document.getElementById("homeB");
