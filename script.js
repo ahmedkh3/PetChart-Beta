7893c0ad1329709ab549b42eebba82aa6f58e573
Zakariya Ahmed <zakariyaahmed077@gmail.com>
2023-10-28 19:03:17 -0400

    Changed Styles and Added new Pages

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const addPetIn = document.getElementById("addPetB");
 const removePetIn = document.getElementById("removePetB");
 const pharmacyB = document.getElementById("pharmacyB");
 const homeB = document.getElementById("homeB");
+
+
