63cac6f54af8d01b13b614ef2a617f51baffdb2f
Ahmed H <hassenahmed2378@gmail.com>
2023-12-15 17:06:14 -0500

    added add pet page

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ const removePetIn = document.getElementById("removePetB");
 const pharmacyB = document.getElementById("pharmacyB");
 const homeB = document.getElementById("homeB");
 
+const myText = document.getElementById("my-text");
+myText.style.cssText = `height: ${myText.scrollHeight}px; overflow-y: hidden`;
+myText.addEventListener("input",function(){
+    this.style.height = "auto";
+    this.style.height = `${this.scrollHeight}px`;
+})
 
+function addTableRow(){
+    const table = document.getElementById("shotHistoryTable");
+    const newRow = table.insertRow();
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    cell1.innerHTML ='<input type="text" class = "infoIn"">';
+    cell2.innerHTML = '<input type = "text" class ="dateIn">';
+
+}
+function deleteTableRow(button) {
+   
+    const row = button.parentNode.parentNode;
+
+    row.parentNode.removeChild(row);
+}
